64cbc293f312edc288feb7770c1ef4ab84dd80f5
ansumana80 <ansurogersbusiness@gmail.com>
2019-06-13 14:29:45 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args)
+    {
+        System.out.println("Cost\tQuantity\tTotal");
+        System.out.println("$1000.00  \t4\t\t$4000.00");
+        System.out.println("$50.00  \t8\t\t$400.00");
+        System.out.println("Total\t\t12\t\t$4400.00");
+
+
+    }
+}
